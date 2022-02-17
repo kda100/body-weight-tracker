dcf9356ed83ccb6bf7132107af3cb5e3b1250ca3
kda100 <65980399+kda100@users.noreply.github.com>
2022-02-17 11:17:18 +0000

    Update datetime_range_helper.dart

--- lib/helpers/datetime_range_helper.dart
+++ lib/helpers/datetime_range_helper.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 /// class containing helpers to use for the DateTimeRange object.
 
 class DateTimeRangeHelper {
+  /// gets the DateTimeRange object with the max and min DateTimes of the current quarter we are in DateTimeRange(01/01/2022, 31/03/2022)
   static DateTimeRange getCurrentQuarterDateRange() {
     final DateTime now = DateTime.now();
     final int currQuarter = ((now.month - 1) / 3 + 1).toInt();
@@ -12,6 +13,7 @@ class DateTimeRangeHelper {
     );
   }
 
+  /// adds a set number of months to the max and min DateTimes of a DateTimeRange Object. 
   static DateTimeRange addMonths({
     required DateTimeRange dateTimeRange,
     required int numMonths,
@@ -31,6 +33,7 @@ class DateTimeRangeHelper {
     return newDateTimeRange;
   }
 
+  /// substracts a set number of months to the max and min DateTimes of a DateTimeRange Object. 
   static DateTimeRange subtractMonths({
     required DateTimeRange dateTimeRange,
     required int numMonths,
