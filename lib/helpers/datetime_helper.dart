f631eb417b5d87249f8d19617e282e4325cc4a43
kda100 <65980399+kda100@users.noreply.github.com>
2022-02-17 11:13:06 +0000

    Update datetime_helper.dart

--- lib/helpers/datetime_helper.dart
+++ lib/helpers/datetime_helper.dart
@@ -4,11 +4,14 @@ import 'package:intl/intl.dart';
 /// class containing helpers to use for the DateTime object.
 
 class DateTimeHelper {
+  
+  ///gets current day
   static DateTime today() {
     DateTime now = DateTime.now();
     return DateTime(now.year, now.month, now.day);
   }
 
+  /// changes "17/02/2022" format -> DateTime Object
   static DateTime? formatDDMMYYYYStringToDateTime(String? dateTime) {
     if (dateTime != null && dateTime.isNotEmpty) {
       DateFormat format = DateFormat("dd/MM/yyyy");
@@ -17,6 +20,7 @@ class DateTimeHelper {
     return null;
   }
 
+  /// changes DateTime Object to "17/02/2022" format
   static String formatDateTimeToDDMMYYYYString(DateTime? dateTime) {
     if (dateTime != null) {
       return '${DateFormat('dd/MM/yyyy').format(dateTime)}';
@@ -24,6 +28,7 @@ class DateTimeHelper {
     return "";
   }
 
+  /// changes DateTime to "Thu 17 Feb 2022" format
   static String formatDateTimeToDayMonthYearString(DateTime? dateTime) {
     if (dateTime != null) {
       return '${DateFormat('EE dd MMM yyyy').format(dateTime)}';
