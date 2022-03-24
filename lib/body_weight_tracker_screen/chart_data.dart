2b0e02d55ba4d75c7199988556569a220691f6fe
kda100 <65980399+kda100@users.noreply.github.com>
2022-03-24 11:22:59 +0000

    Update chart_data.dart

--- lib/body_weight_tracker_screen/chart_data.dart
+++ lib/body_weight_tracker_screen/chart_data.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+///widget used to display different information presented in the body weight tracker chart
+///The selected date, selected weight and target.
+
 class ChartData extends StatelessWidget {
   final String header;
   final Widget data;
