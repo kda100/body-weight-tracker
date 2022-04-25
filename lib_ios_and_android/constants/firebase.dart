2f871c08400d01c566eae48f08017879dbbe4cfe
kda100 <65980399+kda100@users.noreply.github.com>
2022-04-25 08:23:54 +0100

    Add files via upload

---
+++ lib_ios_and_android/constants/firebase.dart
@@ -0,0 +1,8 @@
+const String targetDocRef = "userId/targetDoc";
+const String weightRecordsColRef = "weightRecords";
+
+class FieldNames {
+  static const String dateField = "date";
+  static const String weightField = "weight";
+  static const String targetField = "target";
+}
